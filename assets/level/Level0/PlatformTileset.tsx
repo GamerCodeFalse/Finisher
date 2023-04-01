a483130311f5a251e4a06c77fe34bc2aa0767925
madhuri <madhuri.alladi@gmail.com>
2023-03-31 22:29:08 -0500

    Added More From Past Projects

---
+++ assets/level/Level0/PlatformTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="PlatformTileset" tilewidth="16" tileheight="16" tilecount="1700" columns="68">
+ <image source="../sprites/env/enviorment_tileset.png" width="1100" height="400"/>
+</tileset>
